fe73c3e275f6781a43c32c6473bb359468dc30ff
Marian Negru <marian.negru@gmail.com>
2024-01-09 09:03:53 +0200

    Lint

--- src/main/kotlin/com/hyperdevs/poeditor/gradle/adapters/PoEditorDateJsonAdapter.kt
+++ src/main/kotlin/com/hyperdevs/poeditor/gradle/adapters/PoEditorDateJsonAdapter.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HyperDevs
+ * Copyright 2024 HyperDevs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
